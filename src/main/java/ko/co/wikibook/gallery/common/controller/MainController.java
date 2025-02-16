e93544ad2d7c1256f5302290540413829cd90684
tngkrwlsmd <wjsdudals7590@naver.com>
2025-02-16 11:48:10 +0900

    project setting

--- src/main/java/ko/co/wikibook/gallery/common/controller/MainController.java
+++ src/main/java/ko/co/wikibook/gallery/common/controller/MainController.java
@@ -1,5 +1,15 @@
 package ko.co.wikibook.gallery.common.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
 public class MainController {
 
+    @GetMapping("/api/greeting")
+    public String greet() {
+        return "Hello Spring";
+    }
 }
